8538ef616fde5431ecb1cd80eb1e63feaa55a4d7
Sirojiddin <sirojboy@inbox.ru>
2024-01-27 22:10:08 +0500

    add excs

---
+++ Leksiya/less1/check.java
@@ -0,0 +1,8 @@
+package Leksiya.less1;
+
+public class check {
+    public static void main(String[] args) {
+        int i = 123;
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
---
+++ Leksiya/less1/program.java
@@ -0,0 +1,13 @@
+/*
+ * comment AUE
+ * 
+ */
+
+package Leksiya.less1;
+
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // comment AUE
+        
+    }
+}
