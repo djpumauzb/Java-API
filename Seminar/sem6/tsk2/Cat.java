97b7d3414ede0e6503e969c05410e2ba0df1bd7b
Sirojiddin <sirojboy@inbox.ru>
2024-02-14 20:46:27 +0500

    edt sem6

--- Seminar/sem6/tsk2/Cat.java
+++ Seminar/sem6/tsk2/Cat.java
@@ -1,5 +1,7 @@
 package Seminar.sem6.tsk2;
 
+import java.util.Objects;
+
 /*
 1. Продумайте структуру класса Кот. 
 Какие поля и методы будут актуальны для приложения, которое является
@@ -13,16 +15,15 @@ public class Cat {
     String name;
     String owner;
     String breed;
-
     private int age;
 
     @Override
     public String toString() {
         return "Cat {" +
                 "name='" + name + '\'' +
-                "owner='" + owner + '\'' +
-                "breed='" + breed + '\'' +
-                "age=" + age +
+                ", owner='" + owner + '\'' +
+                ", breed='" + breed + '\'' +
+                ", age=" + age +
                 "}";
     }
 
@@ -31,5 +32,22 @@ public class Cat {
         this.owner = owner;
         this.breed = breed;
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Cat cat = (Cat) obj;
+        return age == cat.age && Objects.equals(name, cat.name) && Objects.equals(owner, cat.owner)
+                && Objects.equals(breed, cat.breed);
+    }
+
+    @Override
+    public int hashCode() {
+        // return Objects.hash(name, owner, breed, age);
+        return 8 * age + 6 * name.hashCode() + 4 * owner.hashCode() + 11 * breed.hashCode();
     }
 }
--- Seminar/sem6/tsk2/Main.java
+++ Seminar/sem6/tsk2/Main.java
@@ -1,8 +1,23 @@
 package Seminar.sem6.tsk2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Murzik", "Ivanov I.I.", "Scottishfold", 3);
-        System.out.println(cat);
+        Cat cat1 = new Cat("Murzik", "Ivanov I.I.", "Scottishfold", 3);
+        Cat cat11 = new Cat("Murzik", "Ivanov I.I.", "Scottishfold", 3);
+        Cat cat2 = new Cat("Merik", "Lomonov O.M.", "Tai", 1);
+        Cat cat3 = new Cat("Barsik", "Petrov I.V.", "British", 2);
+        Set<Cat> set = new HashSet<>();
+        set.add(cat1);
+        set.add(cat2);
+        set.add(cat3);
+        System.out.println(set);
+        System.out.println(cat1);
+        System.out.println(cat1);
+        System.out.println(cat1.equals(cat11));
+        System.out.println(cat1.hashCode());
+        System.out.println(cat11.hashCode());
     }
 }
